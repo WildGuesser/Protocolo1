1cc81035f0f4656da29a912c936490490f4ec3d2
Hugo Mansilha <102358829+WildGuesser@users.noreply.github.com>
2023-04-27 12:24:34 +0100

    6.2 tem soluçao final?

--- Exercicio_6/Exercicio_6.1.py
+++ Exercicio_6/Exercicio_6.1.py
@@ -1,31 +1,65 @@
-# Definir valores para prever
-X_futuro = pd.DataFrame({
-    'Year': [2024, 2025, 2026, 2030],
-    'GDP_per_capita': [1500, 1600, 1700, 2000],
-    'Incidents_HIV': [5, 7, 9, 15]
-})
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
 
-# Normalizar os dados de X_futuro
-X_futuro_norm = pd.DataFrame(scaler.transform(X_futuro), columns=X_futuro.columns)
+# Le o ficheiro
+life_expectancy = pd.read_csv("../Life-Expectancy-Data-Updated.csv")
 
-# Fazer previsões para os anos de 2024, 2025, 2026 e 2030
-y_futuro_norm = model.predict(X_futuro_norm)
+# Seleciona apenas as colunas relevantes
+life_expectancy = life_expectancy[['Year', 'GDP_per_capita', 'Polio', 'Life_expectancy', 'Country']]
 
-# Desnormalizar as previsões
-y_futuro = scaler.inverse_transform(y_futuro_norm.reshape(-1, 1)).flatten()
+# Seleciona só a região de Africa e o Gabão
+life_expectancy = life_expectancy.loc[(life_expectancy.Country == 'Gabon')]
 
-# Mostrar as previsões
-print('Previsões para os anos de 2024, 2025, 2026 e 2030:')
-print(y_futuro)
+# Remove valores ausentes
+life_expectancy.dropna(inplace=True)
 
-# Plotar gráfico de regressão linear para as previsões
-X_completo = pd.concat([X_norm, X_futuro_norm])
-y_completo = np.concatenate([y_norm, y_futuro_norm])
+# Separa os dados em treino e teste, deixando os 3 últimos anos para teste
+X_train = life_expectancy[life_expectancy['Year'] <= 2015][['Year', 'Polio', 'GDP_per_capita']]
+y_train_polio = life_expectancy[life_expectancy['Year'] <= 2015]['Polio']
+y_train_gdp = life_expectancy[life_expectancy['Year'] <= 2015]['GDP_per_capita']
 
-plt.figure(figsize=(10, 6))
-sns.regplot(x='Year', y='Life_expectancy', data=df_norm, ci=None)
-sns.regplot(x=X_completo['Year'], y=y_completo, ci=None, scatter=False)
-plt.title('Regressão Linear para Previsão da Expectativa de Vida na África')
+X_test = np.array([[2016], [2017], [2018]])
+X_test = np.array([[2016, 0, 0], [2017, 0, 0], [2018, 0, 0]])
+
+
+# Cria os modelos de regressão linear
+model_polio = LinearRegression()
+model_gdp = LinearRegression()
+
+# Treina os modelos
+model_polio.fit(X_train, y_train_polio)
+model_gdp.fit(X_train, y_train_gdp)
+
+# Faz as previsões para Polio e GDP_per_capita
+X_test[:, 1] = model_polio.predict(X_test[:, [0, 1, 2]])
+X_test[:, 2] = model_gdp.predict(X_test[:, [0, 1, 2]])
+
+
+# Normaliza as variáveis
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Cria o modelo de regressão linear para a esperança média de vida
+y_train = life_expectancy[life_expectancy['Year'] <= 2015]['Life_expectancy']
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Faz as previsões para os anos de 2016, 2017 e 2018
+X_pred = scaler.transform(X_test)
+y_pred = model.predict(X_pred)
+
+# Cria um gráfico para mostrar as previsões
+years = [2016, 2017, 2018]
+plt.plot(years, y_pred)
+plt.title('Previsão da esperança média de vida para o Gabão')
 plt.xlabel('Ano')
-plt.ylabel('Expectativa de Vida')
+plt.ylabel('Esperança média de vida')
 plt.show()
+
+print("Previsões para a esperança média de vida:")
+for year, prediction in zip(years, y_pred):
+    print(f"{year}: {prediction:.2f}")
